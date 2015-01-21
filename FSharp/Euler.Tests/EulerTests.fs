fbf495a7d8afdefc2bbf2d528ec28e20ccd48185
tjaskula <thomasv1000@hotmail.fr>
2015-01-22 00:35:26 +0100

    Add initial Euler problem 1 solution

---
+++ FSharp/Euler.Tests/EulerTests.fs
@@ -0,0 +1,13 @@
+﻿namespace Euler.Tests
+
+open Xunit
+open FsCheck
+open Euler
+
+module ``Euler problem tests tests`` =
+
+    // Problem 1 : Multiples of 3 and 5
+    [<Fact>]
+    let ``Multiples of 3 and 5``() =
+        let result = problem1 [1..999] = 233168
+        Check.Quick result
---
+++ FSharp/Euler.Tests/Script.fsx
@@ -0,0 +1,16 @@
+﻿// sets the current directory to be same as the script directory
+System.IO.Directory.SetCurrentDirectory (__SOURCE_DIRECTORY__)
+#I @"../packages/FsCheck.1.0.4/lib/net45"
+#I @"../packages/FsCheck.Xunit.1.0.4/lib/net45"
+#I @"../packages/xunit.1.9.2/lib/net20"
+#I @"bin/Debug"
+#r @"FsCheck.dll"
+#r @"FsCheck.XUnit.dll"
+#r @"xunit.dll"
+#r @"Euler.dll"
+
+open FsCheck
+open Euler.Problems
+
+// Problem 1: Multiples of 3 and 5
+let result = problem1 [0..999]
