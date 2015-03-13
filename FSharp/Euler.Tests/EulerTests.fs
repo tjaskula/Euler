9633e74d5cc89e3825230d8ec4b27aee538483c1
tjaskula <thomasv1000@hotmail.fr>
2015-03-13 22:44:06 +0100

    Add problem5 unit test

--- FSharp/Euler.Tests/EulerTests.fs
+++ FSharp/Euler.Tests/EulerTests.fs
@@ -5,39 +5,45 @@ open FsCheck
 open Euler
 
 module ``Euler problem tests tests`` =
-
+ 
     // Problem 1 : Multiples of 3 and 5
     [<Fact>]
-    let ``Multiples of 3 and 5``() =
+    let ``Problem 1: Multiples of 3 and 5``() =
         let result = problem1 [1..999] = 233168
         Check.Quick result
     
     // Problem 1 : Multiples of 3 and 5
     [<Fact>]
-    let ``Multiples of 3 and 5'``() =
+    let ``Problem 1: Multiples of 3 and 5'``() =
         let result = problem1' [1..999] = 233168
         Check.Quick result
-
+ 
     // Problem 1 : Multiples of 3 and 5
     [<Fact>]
-    let ``Multiples of 3 and 5''``() =
+    let ``Problem 1: Multiples of 3 and 5''``() =
         let result = problem1'' [1..999] = 233168
         Check.Quick result
 
     // Problem 2 : Even Fibonacci numbers
     [<Fact>]
-    let ``Even Fibonacci numbers''``() =
+    let ``Problem 2: Even Fibonacci numbers''``() =
         let result = problem2 () = 4613732I
         Check.Quick result
 
     // Problem 3 : Largest prime factor
     [<Fact>]
-    let ``Largest prime factor``() =
+    let ``Problem 3: Largest prime factor``() =
         let result = problem3 600851475143L = 6857L
         Check.Quick result
 
     // Problem 4 : Largest palindrome product
     [<Fact>]
-    let ``Largest palindrome product``() =
+    let ``Problem 4: Largest palindrome product``() =
         let result = problem4 () = 906609
+        Check.Quick result
+
+    // Problem 5 : Smallest multiple
+    [<Fact>]
+    let ``Problem 5: Smallest multiple``() =
+        let result = problem5 () = 232792560
         Check.Quick result
